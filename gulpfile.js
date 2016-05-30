49ab7f5aeaf638e2cc4799d2c9a3287e0cf3511d
Ben Strumeyer <inevergetmail@DeboraharfsiMac.home>
2016-05-30 04:38:37 -0400

    Injected dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
 var gulp = require('gulp');
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./public/css/*.css'], {read: false});
+    
+    var injectOptions = {
+      ignorePath: '/public'  
+    }; 
+        
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './bower_components',
+        ignorePath: '../../'
+    }
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+})
