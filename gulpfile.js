51ff2cf16a870628aa20b6b844cccd50406d86f6
Ben Strumeyer <inevergetmail@DeboraharfsiMac.home>
2016-05-30 05:12:28 -0400

    Revised project structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
     var inject = require('gulp-inject');
     
-    var injectSrc = gulp.src(['./public/css/*.css'], {read: false});
+    var injectSrc = gulp.src(['./assets/css/*.css'], {read: false});
     
     var injectOptions = {
-      ignorePath: '/public'  
+      ignorePath: '/assets'  
     }; 
         
     var options = {
@@ -16,8 +16,8 @@ gulp.task('inject', function() {
         ignorePath: '../../'
     }
     
-    return gulp.src('./src/views/*.html')
+    return gulp.src('./*.html')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
-        .pipe(gulp.dest('./src/views'));
+        .pipe(gulp.dest('./'));
 })
