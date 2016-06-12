013f4c8b357f93ab990b9c7cc0b73d0d75bb451c
Ben Strumeyer <inevergetmail@DeboraharfsiMac.home>
2016-06-11 21:51:13 -0400

    Didn't need to add attribute values. Instead, I modified the directive scope.

--- app/components/home/projects/projectDirective.js
+++ app/components/home/projects/projectDirective.js
@@ -2,6 +2,7 @@ angular.module('portfolio')
     .directive('project', function () {
         return {
             restrict: 'E',
+            scope: false,
             transclude: true,
             templateUrl: '/app/components/home/projects/project.html'
     }
