fa1d3454cbf523535b988ebb6ba2da061e8f5498
Ben Strumeyer <inevergetmail@DeboraharfsiMac.home>
2016-06-16 22:44:10 -0400

    Updated bower.son with ui-router dependency. Also added a temporary projectInfo directive that I probably won't use.

--- app/components/home/projects/project/projectController.js
+++ app/components/home/projects/project/projectController.js
@@ -1,17 +1,12 @@
 angular.module('portfolio')
     .controller("ProjectCtrl", function ($scope, $http) {
         $http.get('data/projects.json')
-    .success(function(data, status, headers, config) {
-        $scope.projects = data;
-        console.log($scope.projects);
-        $scope.parentPath = "/assets/img/";
-            
-        function viewProject(currentProjectName) {
-            $scope.projectName = currentProjectName;
-        }
-        
-    })
-    .error(function(data, status, headers, config) {
-       // Log here. 
+        .success(function(data, status, headers, config) {
+            $scope.projects = data;
+            console.log($scope.projects);
+            $scope.parentPath = "/assets/img/";
+        })
+        .error(function(data, status, headers, config) {
+        // Log here. 
+        });
     });
-});
--- app/components/home/projects/projectInfo/projectInfoDirective.js
+++ app/components/home/projects/projectInfo/projectInfoDirective.js
@@ -1,12 +1,12 @@
 angular.module('portfolio')
-.directive('projectInfo', function() {
-    return {
-        restrict: 'E',
-        scope: false,
-        transclude: true,
-        templateUrl: '/app/components/home/projects/projectInfo/projectInfo.html',
-        link: function(scope, elem, attrs) {
-            console.log(scope.projects);
+    .directive('projectInfo', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            transclude: true,
+            templateUrl: '/app/components/home/projects/projectInfo/projectInfo.html',
+            link: function(scope, elem, attrs) {
+                console.log(scope.projects);
         }
-    }
+    };
 });
