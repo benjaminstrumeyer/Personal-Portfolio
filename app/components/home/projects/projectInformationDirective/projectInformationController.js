9f67e47c5b2cba165562075696aed2c42031a1e6
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-12-13 19:43:06 -0500

    Injected  projectService into projectInformationDirective.

--- app/components/home/projects/projectInformationDirective/projectInformationController.js
+++ app/components/home/projects/projectInformationDirective/projectInformationController.js
@@ -1,4 +1,7 @@
 app
     .controller('ProjectInformationController', function() {
-         
+        
+        var self = this;
+    
+    
 });
--- app/components/home/projects/projectInformationDirective/projectInformationDirective.js
+++ app/components/home/projects/projectInformationDirective/projectInformationDirective.js
@@ -1,3 +1,10 @@
-app.component('projectInformationComponent', {
-    
+app.directive('projectInformationDirective', function (projectService) {
+    return {
+        restrict: 'E', 
+        templateUrl: 'app/components/projects/projectInformationDirective/projectInformation.html',
+        controller: 'ProjectInformationController', 
+        link: function(scope, elem, attr) {
+            scope.project = projectService.projects;
+        }   
+    }
 });
