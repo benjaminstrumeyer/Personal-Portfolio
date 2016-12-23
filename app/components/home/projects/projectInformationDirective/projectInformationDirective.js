5eb1d790ff3ca431e43c461c191ea1879f18c674
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-12-23 12:52:34 -0500

    Created and placed carousel correctly. Still need to modify buttons and create modal on click. Also changed project-description font size to viewport units for responsive design.

--- app/components/home/projects/projectInformationDirective/projectInformationDirective.js
+++ app/components/home/projects/projectInformationDirective/projectInformationDirective.js
@@ -4,6 +4,14 @@ app.directive('projectInformationDirective', function (projectService, $statePar
         templateUrl: 'app/components/home/projects/projectInformationDirective/projectInformation.html',
         link: function(scope, elem, attr) {
             scope.project = projectService.projects.find(project => project.name === $stateParams.name);
+            
+            $('.carousel-control.left').click(function() {
+                $('#myCarousel').carousel('prev');
+            });
+
+            $('.carousel-control.right').click(function() {
+              $('#myCarousel').carousel('next');
+            });
         }   
     }
 });
