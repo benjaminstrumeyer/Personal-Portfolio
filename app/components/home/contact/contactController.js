c2b4a26202cdd228a8017f0988082c28d56c6c31
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-01-02 18:32:12 -0500

    Bootstrapped contact section and centered icons with contact information text.

--- app/components/home/contact/contactController.js
+++ app/components/home/contact/contactController.js
@@ -4,7 +4,11 @@ app.
     var self = this;
     
     var init = function() {
-        self.motiveDescription ="Currently looking for a team to grow on.  I'd love to hear about your company.";
+        self.title = "Let's Get in Touch";
+        self.motiveDescription = "\"Currently looking for a team to grow in.  I'd love to hear about your company.\"";
+        self.email = "benjamin.strumeyer@gmail.com";
+        self.github = "benstrumeyer";
+        self.resume = "Resume";
     }
     
     init();
