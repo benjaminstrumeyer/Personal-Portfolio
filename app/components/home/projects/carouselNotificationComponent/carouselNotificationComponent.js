48d1124cf83fc78b000a451f744e6d09af23b252
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-01-01 22:01:11 -0500

    Created carousel-notification component.

---
+++ app/components/home/projects/carouselNotificationComponent/carouselNotificationComponent.js
@@ -0,0 +1,5 @@
+app.component('carouselNotificationComponent', {
+    restrict: 'E',
+    templateUrl: 'app/components/home/projects/carouselNotificationComponent/carouselNotification.html',
+    controller: 'carouselNotificationController'
+});
---
+++ app/components/home/projects/carouselNotificationComponent/carouselNotificationController.js
@@ -0,0 +1,10 @@
+app.
+controller('carouselNotificationController', function() {
+    var self = this;
+    
+    var init = function() {
+        self.notificationMessage = "Click for Fullscreen View"
+    }
+    
+    init();
+});
