3d0ca8c42d1a88df1cb7d69a14c091f4ac3b37c4
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-02-07 11:43:51 -0500

    Deleted some comments and refactored code.

--- app/components/home/contact/contactController.js
+++ app/components/home/contact/contactController.js
@@ -3,13 +3,10 @@ app.
     
     var self = this;
     
-    var init = function() {
-        self.title = "Let's Get in Touch";
-        self.motiveDescription = "\"Currently looking for a team to grow in.  I'd love to hear about your company.\"";
-        self.email = "benjamin.strumeyer@gmail.com";
-        self.github = "benstrumeyer";
-        self.resume = "Resume";
-    }
+    self.title = "Let's Get in Touch";
+    self.motiveDescription = "Currently looking for a team to grow in. I'd love to hear about your company.";
+    self.email = "benjamin.strumeyer@gmail.com";
+    self.github = "benstrumeyer";
+    self.resume = "Resume";
     
-    init();
 });
