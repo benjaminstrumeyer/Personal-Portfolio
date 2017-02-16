47db27ca16a02c4d3bfce83ee00ee1da84a75ded
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-02-15 22:47:43 -0500

    Deleted about-me-component contents.

--- app/components/home/about/aboutMeComponent/aboutMeController.js
+++ app/components/home/about/aboutMeComponent/aboutMeController.js
@@ -3,9 +3,5 @@ app
     
     var self = this;
     
-    var init = function() {
-        self.aboutMeDescription = "My name is Ben Strumeyer. I love getting out of my comfort zone whether it be finding a new surf spot, or getting my feet wet with a new framework. Improving myself at every opportunity is what I thrive for. I grew up being taught that if you hit a thousand forehands a day on the tennis court, you'll inevitably be a better version of yourself at the next tournament. I then took that same approach to skateboarding and broke my leg within the first week. Web development's a little safer than skateboarding, so it's been working out so far.";   
-    }
     
-    init();
 });
