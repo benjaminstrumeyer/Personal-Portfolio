43317fb64fddf398322c5c94bc26e413923d552b
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-02-16 23:42:34 -0500

    Computed respective circle x and y coordinates aside from the expected individual circle dimension offset. Passed these variables into the hobbyCircle directive. Also moved link function code to the controller.

--- app/components/home/about/hobbyCircleDirective/hobbyCircleController.js
+++ app/components/home/about/hobbyCircleDirective/hobbyCircleController.js
@@ -3,5 +3,34 @@ app
     
     var self = this;
     
-    
+    var radius = 400;
+            var angle = 0;
+            
+            //TODO: Get testArray from scope. 
+            var testArray = ['test1','test2','test3','test4'];
+            var hobbyLength = testArray.length;
+            
+            var circleAngleIncrements = (2*Math.PI)/hobbyLength;
+            
+            //Offset it if hobbyLength is even to ensure even placemment. 
+//            if (hobbyLength % 2 === 0) {
+//                angle += circleAngleIncrements/2;
+//            }
+            
+            var width = $('.hobby-circle-parent-container').width();
+            var height = $('.hobby-circle-parent-container').height();
+            
+            
+            self.hobbyCircles = "";
+            
+            //Append directives to hobbyCircles with different values all at once with a single ng-html binding. 
+            testArray.forEach(function() {
+                let x = Math.round(width/2 + (radius * Math.cos(angle)));
+                let y = Math.round(height/2 + (radius * Math.sin(angle)));
+                
+                hobbyCircles+= "<hobby-directive x=\"" + x + "\" y=\"" + y + "\"></hobby-directive>";
+                
+                console.log('X, Y: ', x, y);
+                angle += circleAngleIncrements;
+            });
 });
--- app/components/home/about/hobbyCircleDirective/hobbyCircleDirective.js
+++ app/components/home/about/hobbyCircleDirective/hobbyCircleDirective.js
@@ -6,27 +6,6 @@ app.directive('hobbyCircleDirective', function() {
         },
         templateUrl: 'app/components/home/about/hobbyCircleDirective/hobbyCircle.html',
         controller: 'hobbyCircleController',
-        controllerAs: '$ctrl',
-        link: function(scope, attr, elem) {
-            var radius = 400;
-            
-            //TODO: Get testArray from scope. 
-            var testArray = ['test1','test2','test3','test4'];
-            var hobbyLength = testArray.length;
-            
-            var circleAngleIncrements = 360/hobbyLength;
-            //Offset it if hobbyLength is even
-            
-            var height = $('.hobby-circle-parent-container').height();
-            var width = $('.hobby-circle-parent-container').width();
-            
-            
-            var hobbyCircles = "";
-            
-            //Append directives to hobbyCircles with different values all at once with a single ng-html binding. 
-            testArray.forEach(function() {
-                
-            });
-        }
+        controllerAs: '$ctrl'
     }
 });
