831c0afa405cadcaa11e9d9eff5bafc13ae61a3f
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-02-16 21:43:56 -0500

    Fixed about-me.html reference in controller.

--- app/components/home/about/aboutMeComponent/aboutMeComponent.js
+++ app/components/home/about/aboutMeComponent/aboutMeComponent.js
@@ -1,5 +1,5 @@
 app.component('aboutMeComponent', {
     restrict: 'E',
-    templateUrl: 'app/components/home/aboutMeComponent/aboutMe.html',
+    templateUrl: 'app/components/home/about/aboutMeComponent/aboutMe.html',
     controller: 'aboutMeController'
 });
