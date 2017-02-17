00f1042352cd7e41f68b04173e8b02778935cdc8
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-02-16 21:41:42 -0500

    Added hobby-circle-directive to aboutMe.html for future use.

--- app/components/home/about/aboutMeComponent/aboutMeController.js
+++ app/components/home/about/aboutMeComponent/aboutMeController.js
@@ -1,4 +1,6 @@
 app.controller('aboutMeController', function() {
      var self = this;
+    //TODO: Should access a hobby service to put on the scope. 
+    // This should be passed through to the hobbyCircleDirective. 
     
 });
