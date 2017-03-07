66b9142dd11070d369f54f8a406259afdefb83b5
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2017-03-06 21:04:07 -0500

    When a project is clicked, it immediately scrolls to the top of the project section to display the expanded project container vertically centered.

--- app/components/home/projects/projectComponent/projectController.js
+++ app/components/home/projects/projectComponent/projectController.js
@@ -1,30 +1,12 @@
 app
     .controller("ProjectController", function ($scope, $http) {
-//        $http({
-//            url: 'data/projects.json',
-//            method: 'get'
-//        })
-//        .success(function(data, status, headers, config) { 
-//            $scope.projects = data;
-//            $scope.parentPath = "/assets/img/";
-//            
-////            $scope.currentProject = getProject(projectName, data);
-//            
-//        })
-//        .error(function(data, status, headers, config) {
-//        // Log here. 
-//        });
-//    
-//        function getProject(projectName, projects) {
-//            var project = projects.filter(x => x.name.toLowerCase() === projectName.toLowerCase())[0];
-//            return project;
-//        }
+    
     var self = this;
     
-    var init = function() {
-        
-        self.projectInfo = true;
+//    self.projectInfo = true;
+    
+    $scope.scrollTop = function() {
+        $('html, body').scrollTop($('#projects').position().top);  
     }
     
-    init();
     });
