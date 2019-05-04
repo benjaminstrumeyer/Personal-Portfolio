cc1441f2a69c0cd8cad0c4f6b85ceb54be309f2a
Ben <ben.strumeyer@gmail.com>
2019-05-04 15:01:43 -0400

    Typo
    typo

--- app/components/home/contact/contactController.js
+++ app/components/home/contact/contactController.js
@@ -4,7 +4,7 @@ app.
     var self = this;
     
     self.title = "Let's Get in Touch";
-    self.motiveDescription = "Hi there! I'm always up for a chat about new opportunties or tech in general. Shoot me an email, I'm friendly!";
+    self.motiveDescription = "Hi there! I'm always up for a chat about new opportunities or tech in general. Shoot me an email, I'm friendly!";
     self.email = "ben.strumeyer@gmail.com";
     self.github = "benstrumeyer";
     self.resume = "Resume";
